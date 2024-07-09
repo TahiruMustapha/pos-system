b61e7a8d7d9df6cf8c85b2558b76e5e03e03773d
Mustapha tahiru <tahirumustapha13@gmail.com>
2024-07-09 17:07:46 +0000

    change

--- app/clientsProductsPage/success/page.js
+++ app/clientsProductsPage/success/page.js
@@ -2,7 +2,7 @@ import SuccessPage from "@/components/SuccessPage";
 import React from "react";
 
 const Page = () => {
-  // console.log("Hello world");
+  
   return (
     <div>
       <SuccessPage />
